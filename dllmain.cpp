6d1d719c1b8cdbcbefd77c28cd905f2fb338b21e
DalasK <89702597+DalasK@users.noreply.github.com>
2021-10-26 19:45:25 +0200

    Update dllmain.cpp

--- dllmain.cpp
+++ dllmain.cpp
@@ -4,7 +4,7 @@
 
 extern "C" __declspec(dllexport)
 
-DWORD WINAPI MessageBoxThread(LPVOID lpParam) {
+DWORD WINAPI MessageBoxThread(LPVOID lpParam) { //MessageBox appellée dans DllMain sous la forme d'un thread.
 	
   MessageBox(NULL, "Coucou, l'injection a bien fonctionné! ", "Dll-test", MB_SYSTEMMODAL | MB_OK | MB_ICONINFORMATION);
   
@@ -14,11 +14,11 @@ DWORD WINAPI MessageBoxThread(LPVOID lpParam) {
 
 extern "C" __declspec(dllexport)
 
-BOOL APIENTRY DllMain(HMODULE hModule, DWORD ul_reason_for_call, LPVOID lpReserved) { // execute cette partie du code que si la dll est chargée en mémoire 
+BOOL APIENTRY DllMain(HMODULE hModule, DWORD ul_reason_for_call, LPVOID lpReserved) { // execute cette partie du code que si la dll est chargée en mémoire.
  
   switch (ul_reason_for_call) {
     case DLL_PROCESS_ATTACH:
-      CreateThread(NULL, NULL, MessageBoxThread, NULL, NULL, NULL); // appel sous la frome de thread de notre messagebox
+      CreateThread(NULL, NULL, MessageBoxThread, NULL, NULL, NULL); 
       break;
       
     case DLL_THREAD_ATTACH:
